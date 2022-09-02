c53c77a0488ed8094364ffe106df15fc49d1bac9
Victor Retamal <99215206+RetamalVictor@users.noreply.github.com>
2022-09-02 10:29:11 +0200

    Template added for Slurm

--- submit.sh
+++ submit.sh
@@ -1,17 +1,17 @@
 
 #!/bin/bash
 #
-#SBATCH --job-name=experiment-3-attention
+#SBATCH --job-name=<Exp name>
 #SBATCH --partition=fat
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=17
-#SBATCH --mail-user=victorrtml22@gmail.com
+#SBATCH --mail-user=<mail here>
 #SBATCH --mail-type=ALL
-#SBATCH --chdir="/home/baierh/tu-eind-AGSMCTS/"
-#SBATCH --output=/home/baierh/outputs/test4-%j.out
-#SBATCH --error=/home/baierh/outputs/test4-%j.err
+#SBATCH --chdir="<your pwd here>"
+#SBATCH --output=<out path>
+#SBATCH --error=<error path>
 #SBATCH --time=00:08:00
 
 
@@ -19,6 +19,6 @@ module load 2021
 module load Python/3.9.5-GCCcore-10.3.0
 
 source ./.research/bin/activate
-cd ./tu-eind-AGSMCTS/src/learning/
+cd ./A3C-Attention-for-Simultaneous-game/src/learning
 python -u learning.py --nb-processes 16 --nb-players 4 --nb-soft-attention-heads 5 --hard-attention-rnn-hidden-size 128
 
